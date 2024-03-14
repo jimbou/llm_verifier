a251ddf42f769c6f3814b59935693ed63e244396
jimbou <el17072@mail.ntua.gr>
2024-03-14 19:04:07 +0200

    docker compose changes

--- commands.sh
+++ commands.sh
@@ -5,3 +5,5 @@ klee --libc=uclibc test1.bc &>help.txt
 docker container start klee_docker
 docker cp error_report.txt klee_docker:/home/klee
 docker exec klee_docker cp error_report.txt error_report2.txt
+
+docker-compose up --build
--- verification.sh
+++ verification.sh
@@ -5,11 +5,17 @@ docker exec klee_docker rm -f verification/klee_analysis.c
 docker cp klee_analysis.c klee_docker:/home/klee/verification
 docker exec klee_docker clang -c -g -emit-llvm verification/klee_analysis.c
 docker exec klee_docker mv klee_analysis.bc verification
-# docker exec klee_docker rm -f verification/help.txt
+docker exec klee_docker rm -f klee_results.txt
 # docker exec klee_docker touch verification/help.txt
-docker exec klee_docker  /bin/sh -c "klee --libc=uclibc verification/klee_analysis.bc" &> klee_results.txt
+docker exec klee_docker /bin/sh -c "script -q -c 'timeout 15 klee --libc=uclibc verification/klee_analysis.bc' /dev/null > klee_results.txt 2>&1"
+rm -f klee_results.txt
+docker cp klee_docker:/home/klee/klee_results.txt .
 docker exec klee_docker /bin/sh -c "cd verification && ./run.sh"
+rm -f error_report_klee.txt
 docker cp klee_docker:/home/klee/verification/output.txt error_report_klee.txt
 
+docker container stop klee_docker
 
-docker run -it -v $PWD:/infer-examples infer_image /bin/bash -c "cd infer-examples; infer -- clang -c infer_analysis.c >infer_report.txt"
+
+rm -f infer_report.txt
+docker run  --rm -it -v $PWD:/infer-examples infer_image /bin/bash -c "cd infer-examples; infer -- clang -c infer_analysis.c >infer_report.txt"
